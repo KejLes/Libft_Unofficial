a4073f5c00a5d89d0b26a09f6c8e8c99b9b143e3
kej_les <joaquincanales45@gmail.com>
2025-05-26 13:40:04 +0200

    perfecto xd, faltan los leaks

--- ft_split.c
+++ ft_split.c
@@ -5,14 +5,88 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kcanales <kcanales@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/20 09:56:29 by kcanales          #+#    #+#             */
-/*   Updated: 2025/05/23 16:57:36 by kcanales         ###   ########.fr       */
+/*   Created: 2025/05/26 10:51:00 by kcanales          #+#    #+#             */
+/*   Updated: 2025/05/26 10:55:47 by kcanales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	words_count(char *s, char c)
+static size_t	words_count(char const *s, char c);
+static void		put_in_word_list(const char *s, char c,	char **ptr_strs);
+static void		free_arr_of_strs(char **arr, size_t words);
+
+char	**ft_split(char const *s, char c)
+{
+	char	**ptr_strs;
+	size_t	words;
+
+	words = words_count(s, c);
+	ptr_strs = calloc((words + 1), sizeof(char *));
+	if (!ptr_strs)
+	{
+		free(ptr_strs);
+		return (NULL);
+	}
+	put_in_word_list(s, c, ptr_strs);
+	if (ptr_strs == NULL)
+	{
+		free_arr_of_strs(ptr_strs, words);
+		return (NULL);
+	}
+	return (ptr_strs);
+}
+
+int	main(void)
+{
+	char	a;
+	char	*b = &a;
+	char	**c = &b;
+	c = ft_split("Hola!,,,,,,,,,,,", ',');
+	for (int i = 0; i < 3; i++)
+		printf("%s\n", c[i]);
+	free_arr_of_strs(c, 3);
+}
+
+static void	free_arr_of_strs(char **arr, size_t words)
+{
+	size_t	ifree;
+
+	ifree = 0;
+	while (ifree < words)
+	{
+		free(arr[ifree]);
+		ifree++;
+	}
+	free(arr);
+}
+
+static void	put_in_word_list(const char *s, char c, char **ptr_strs)
+{
+	size_t	i;
+	size_t	start;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (i < ft_strlen(s))
+	{
+		if (s[i] != c)
+		{
+			start = i;
+			while ((s[i + 1] != c) && (s[i + 1] != '\0'))
+				i++;
+			ptr_strs[j] = ft_substr(s, (start), (i - start + 1));
+			if (ptr_strs[j] == NULL)
+				break ;
+			j++;
+		}
+		i++;
+	}
+	ptr_strs[j] = NULL;
+}
+
+size_t	words_count(char const *s, char c)
 {
 	size_t	i;
 	size_t	words_count;
@@ -26,13 +100,4 @@ static int	words_count(char *s, char c)
 		i++;
 	}
 	return (words_count);
-}
-
-char **ft_split(char const *s, char c)
-{
-	char	**ptr_strs;
-	size_t	words_count;
-		
-	ptr_strs = malloc(words_count * sizeof(char *));
-	
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kcanales <kcanales@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 13:38:20 by kcanales          #+#    #+#             */
-/*   Updated: 2025/05/23 16:40:22 by kcanales         ###   ########.fr       */
+/*   Updated: 2025/05/26 10:51:46 by kcanales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-// char	**ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
