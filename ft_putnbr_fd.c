e6e77dd62c1ba666547ea928ff5f7ed615592be9
kej_les <joaquincanales45@gmail.com>
2025-05-28 14:44:38 +0200

    act norminette ok

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,32 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kcanales <kcanales@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/23 16:33:11 by kcanales          #+#    #+#             */
-/*   Updated: 2025/05/28 14:06:45 by kcanales         ###   ########.fr       */
+/*   Created: 2025/05/28 14:41:20 by kcanales          #+#    #+#             */
+/*   Updated: 2025/05/28 14:44:16 by kcanales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
+static long int	power(long int a);
+static long int	int_len(long int n);
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	int	nb;
+	long int	ln;
+	char		intprint;
 
-	nb = (int)n;
-	if (nb < 0)
+	ln = (long int)n;
+	if (ln < 0)
 	{
 		write(fd, "-", 1);
-		nb = -nb;
+		ln = -ln;
 	}
-	if (nb > 9)
+	while (int_len(ln) > 1)
 	{
-		ft_putnbr_fd(nb / 10, fd);
-		ft_putchar_fd('0' + nb % 10, fd);
+		intprint = ((ln / power(int_len(ln) - 1)) + 48);
+		write(fd, &intprint, 1);
+		ln = ln % power((int_len(ln) - 1));
 	}
-	else
-		ft_putchar_fd('0' + nb, fd);
+	intprint = (ln + 48);
+	write(fd, &intprint, 1);
 }
-*/
 
 static long int	int_len(long int n)
 {
@@ -49,7 +53,7 @@ static long int	int_len(long int n)
 
 static long int	power(long int a)
 {
-	long int i;
+	long int	i;
 
 	i = 1;
 	while (a != 0)
@@ -60,40 +64,27 @@ static long int	power(long int a)
 	return (i);
 }
 
-void	ft_putnbr_fd(int n, int fd)
-{
-	long long int	ln;
-	char		intprint;
-	long int	a, b, c, d;
-
-	ln = (long long int)n;
-	if (ln < 0)
-	{
-		write(fd, "-", 1);
-		ln = -ln;
-	}
-	while (ln != 0)
-	{
-		a = int_len(ln) - 1;
-		b = power(a);
-		c = ln /b;
-		d = c + 48;
-		intprint = d;//((n/(10 * (int_len(n) - 1))) + 48);
-		write(fd, &intprint, 1);
-		ln = ln % (b);
-		if (ln < 10)
-		{
-			intprint = (ln + 48);
-			write(fd, &intprint, 1);
-			break;
-		}
-	}
-}
-
+/*
 int	main(void)
 {
-	// int	a = -214748364;
-	int a = -2147483648;
+	int	a = -2147483647;
 	ft_putnbr_fd(a, 1);
-	printf("\n%d!\n", a);
+	printf("\n%d!\n\n", a);
+
+	int b = -2147483648;
+	ft_putnbr_fd(b, 1);
+	printf("\n%d!\n\n", b);
+
+	int c = 0;
+	ft_putnbr_fd(c, 1);
+	printf("\n%d!\n\n", c);
+
+	int d = 1;
+	ft_putnbr_fd(d, 1);
+	printf("\n%d!\n\n", d);
+
+	int e = -0;
+	ft_putnbr_fd(e, 1);
+	printf("\n%d!\n\n", e);
 }
+*/
